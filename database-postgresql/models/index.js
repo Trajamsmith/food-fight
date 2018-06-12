864a7e5327f431d0e097f031e389a0e1dc77a36a
Travis James Smith <trajamsmith@gmail.com>
2018-06-12 16:43:47 -0500

    Update to use Google Cloud SQL

--- database-postgresql/models/index.js
+++ database-postgresql/models/index.js
@@ -7,9 +7,9 @@ const { Op } = Sequelize;
 // set up connection and create sequelize instance
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  // port: process.env.DB_PORT,
   dialect: 'postgres',
-  operatorsAliases: false,
+  // operatorsAliases: false,
   logging: false,
   // dialectOptions: {
   //   ssl: true,
